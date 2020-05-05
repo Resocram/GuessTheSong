40b8560d37bea190bcd7fb92f0bb57df8a6f51dc
austin <marcoser2000@gmail.com>
2020-05-04 19:46:19 -0700

    Updates

---
+++ precache-manifest.ba9907124b307e208b2a40c1f4750455.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b793ab7d225dc1a094d78d3aaa6f4ae1",
+    "url": "/GuessTheSong/index.html"
+  },
+  {
+    "revision": "c81049e2725079cbbeae",
+    "url": "/GuessTheSong/static/css/main.0f176723.chunk.css"
+  },
+  {
+    "revision": "598b89891f50bc8d9237",
+    "url": "/GuessTheSong/static/js/2.bfc8628e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GuessTheSong/static/js/2.bfc8628e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81049e2725079cbbeae",
+    "url": "/GuessTheSong/static/js/main.e31fc2ec.chunk.js"
+  },
+  {
+    "revision": "b9c55273a23904469005",
+    "url": "/GuessTheSong/static/js/runtime-main.eaea4b10.js"
+  },
+  {
+    "revision": "aa56a748092d87acadaf964342a07f72",
+    "url": "/GuessTheSong/static/media/80s.aa56a748.jpg"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/GuessTheSong/static/media/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "17ba6410cbc694808961a988fd4426de",
+    "url": "/GuessTheSong/static/media/Raleway-SemiBold.17ba6410.ttf"
+  },
+  {
+    "revision": "dc581dd00b5f8d840c56bbdfbecc289c",
+    "url": "/GuessTheSong/static/media/classical.dc581dd0.jpg"
+  },
+  {
+    "revision": "ed025b7308c372279cb42e513d1ffcf5",
+    "url": "/GuessTheSong/static/media/country.ed025b73.jpg"
+  },
+  {
+    "revision": "89b94847f7ddbaebeb4bbf8b2479c276",
+    "url": "/GuessTheSong/static/media/electronic.89b94847.jpg"
+  },
+  {
+    "revision": "0d41688418b2a48750528cbea4e7e3ff",
+    "url": "/GuessTheSong/static/media/metal.0d416884.jpg"
+  },
+  {
+    "revision": "eee32452177448abf31f210aba0cd089",
+    "url": "/GuessTheSong/static/media/pop.eee32452.jpg"
+  },
+  {
+    "revision": "ad38e4236697fe6281f4b26a4a6c2ba3",
+    "url": "/GuessTheSong/static/media/punk.ad38e423.jpg"
+  },
+  {
+    "revision": "f6d198254a7325c3ddfa4923e6aecb07",
+    "url": "/GuessTheSong/static/media/rap.f6d19825.jpg"
+  },
+  {
+    "revision": "f8960f78dbce7483b9d99f53b95d16ce",
+    "url": "/GuessTheSong/static/media/rnb.f8960f78.jpg"
+  },
+  {
+    "revision": "b28ddfe7f6d56f17100c665a38026f59",
+    "url": "/GuessTheSong/static/media/rock.b28ddfe7.jpg"
+  }
+]);
